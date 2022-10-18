238b67df1fe1343067124d24f2daa6365aff623c
Andresdvp <96126942+Andresdvp@users.noreply.github.com>
2022-10-18 18:31:41 -0500

    fin clase 8

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-     <h1>tienda naturista </h1>
-    <img src="/images/eco.png" alt='aqui va el logo'></img>
+        <Header/>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <Footer/>
     </div>
   );
 }
