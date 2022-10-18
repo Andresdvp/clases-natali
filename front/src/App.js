6cdff67e8c0ca1379f95c3c15d8cd123a3866f9b
Andresdvp <96126942+Andresdvp@users.noreply.github.com>
2022-10-18 15:31:37 -0500

    fin clase 7

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>tienda naturista </h1>
+    <img src="/images/eco.png" alt='aqui va el logo'></img>
+    </div>
+  );
+}
+
+export default App;
